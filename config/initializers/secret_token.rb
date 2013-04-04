c9eff78df077f786878cd69f2b26c86141f5c23d
BPluribus81 <ender081@gmail.com>
2013-04-03 21:15:15 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PluribusClean::Application.config.secret_token = 'f5b1314d048942af202110902ac054543c6eb9f649dc73321b79e9b0fa045091a8e229c08c711831670e4ef2cbf37eda725bd23cd7c5c15cdeefd2b60596785b'
